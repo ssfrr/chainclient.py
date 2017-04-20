a637125402ea03d2ffae39a104a5eabde9122b1b
Spencer Russell <spencer.f.russell@gmail.com>
2017-04-20 11:13:29 -0400

    bumps version to 0.5.0

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
     name='chainclient',
-    version='0.4.0',
+    version='0.5.0',
     description='A Python client for the Chain API',
     py_modules=['chainclient'],
     author='Spencer Russell',
-    author_email='sfr@mit.edu',
+    author_email='sfr@media.mit.edu',
     url='http://github.com/ssfrr/chainclient.py',
     license='MIT',
     install_requires=['requests'],
